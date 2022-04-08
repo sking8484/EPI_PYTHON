3b222990e7082372e327b5a0c814c1c112b8818a
Darst King <darstking@Darsts-MacBook-Pro.local>
2022-04-07 21:47:24 -0700

    Finished int_as_aray_multiply Program

--- epi_judge_python/int_as_array_multiply.py
+++ epi_judge_python/int_as_array_multiply.py
@@ -4,8 +4,20 @@ from test_framework import generic_test
 
 
 def multiply(num1: List[int], num2: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+
+    sign = -1 if (num1[0] < 0) ^ (num2[0] < 0) else 1
+    num1[0],num2[0] = abs(num1[0]),abs(num2[0])
+    results = [0] * (len(num1) + len(num2))
+
+    for i in reversed(range(len(num1))):
+        for j in reversed(range(len(num2))):
+            
+            results[i + j + 1] += num1[i] * num2[j]
+            results[i + j] += results[i + j + 1] // 10
+            results[i + j + 1] %= 10
+    
+    results = results[next((i for i,x in enumerate(results) if x != 0),len(results)):] or [0]
+    return [sign*results[0]] + results[1:]
 
 
 if __name__ == '__main__':
