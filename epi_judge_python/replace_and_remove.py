fb974085789434a9b5a85922478e9be8dd872bf0
Darst King <darstking@Darsts-MacBook-Pro.local>
2022-04-19 13:45:38 -0700

    Finished replace_and_remove

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -5,9 +5,38 @@ from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
 
+'''Pass 1: If its a b then overwrite the last b with this letter. If its an a then update a count. Thats first pass. 
+Pass 2: Since write index will be one greater than what it should be, set curr index to write index -1, set the write index to write index + a -1
+    total cound is write index + 1 now. 
+
+    first write index shows how many non bs we have. Second write index, gives us how many non bs as well as double as. 
+    Curr index is going to be the last element in length, and write_idx is now where we write to with larger as. 
+'''
+
 def replace_and_remove(size: int, s: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+    write_idx, a_count = 0,0
+    for i in range(size):
+        if s[i] != 'b':
+            s[write_idx] = s[i]
+            write_idx += 1
+        if s[i] == 'a':
+            a_count += 1
+    
+    curr_idx = write_idx - 1
+    write_idx += a_count -1
+    total_count = write_idx + 1
+
+    while curr_idx >= 0:
+        if s[curr_idx] == 'a':
+            s[write_idx -1:write_idx + 1] = 'dd'
+            write_idx -= 2
+        else:
+            s[write_idx] = s[curr_idx]
+            write_idx -= 1
+        curr_idx -= 1
+            
+
+    return total_count
 
 
 @enable_executor_hook
